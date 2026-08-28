40cee63d5f51f3843b4c67b6ef61011291037439
MintFlavored <mint1@duck.com>
2026-08-29 02:47:05 +0300

    fix(tauri): use 127.0.0.1 and fix permission identifiers in default capabilities

--- packages/app/vite.config.ts
+++ packages/app/vite.config.ts
@@ -5,12 +5,13 @@ export default defineConfig({
   plugins: [react()],
   clearScreen: false,
   server: {
+    host: "127.0.0.1",
     port: 1420,
     strictPort: true,
   },
   envPrefix: ["VITE_", "TAURI_"],
   build: {
-    target: process.env.TAURI_ENV_PLATFORM == "windows" ? "chrome105" : "safari13",
+    target: process.env.TAURI_ENV_PLATFORM === "windows" ? "chrome105" : "safari13",
     minify: !process.env.TAURI_ENV_DEBUG ? "esbuild" : false,
     sourcemap: !!process.env.TAURI_ENV_DEBUG,
   },
